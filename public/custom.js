1c841eb0006e99bb5626c9887e102fbb8e751d5f
Jeremy Spiegel <jeremy.s.spiegel@gmail.com>
2013-12-12 00:47:28 -0800

    Add modal dialog for showing review/synopsis when you click on a movie.

--- public/custom.js
+++ public/custom.js
@@ -1,6 +1,6 @@
 angular.module('app', ['ui.bootstrap']);
 
-function StixController($scope, $http) {
+function StixController($scope, $http, $modal) {
     $http.get('/data')
     .success(function(data, status, headers, config) {
         $scope.data = data;
@@ -9,4 +9,27 @@ function StixController($scope, $http) {
     .error(function(data, status, headers, config) {
         $scope.error = data || "Request failed";
     });
+
+    $scope.onclick = function(movie) {
+        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/' + movie.id + '.json?page_limit=50&apikey=3h8aduxd8tx6k72gcmfmj3hw&callback=JSON_CALLBACK')
+        .success(function(movie) {
+            console.log(movie);
+            $modal.open({
+                templateUrl: 'modal.html',
+                controller: ModalController,
+                resolve: {
+                    movie: function() {
+                        return movie;
+                    }
+                }
+            });
+        })
+        .error(function(data, status, headers, config) {
+            $scope.error = data || "Request failed";
+        });
+    };
+}
+
+function ModalController($scope, movie) {
+    $scope.movie = movie;
 }
