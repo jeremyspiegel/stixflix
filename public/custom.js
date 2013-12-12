ad10bb33979db55997bd9cb1c71d48325c16b086
Jeremy Spiegel <jeremy.s.spiegel@gmail.com>
2013-12-12 13:03:53 -0800

    There are so few new releases/upcoming, so just put them at the top above all movies.

--- public/custom.js
+++ public/custom.js
@@ -1,8 +1,6 @@
 angular.module('app', ['ui.bootstrap', 'infinite-scroll']);
 
 function StixController($scope, $http, $modal) {
-    $scope.activeTab = {};
-
     $http.get('/data')
     .success(function(data, status, headers, config) {
         $scope.data = data;
@@ -12,7 +10,7 @@ function StixController($scope, $http, $modal) {
     });
 
     $scope.onclick = function(movie) {
-        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/' + movie.id + '.json?page_limit=50&apikey=3h8aduxd8tx6k72gcmfmj3hw&callback=JSON_CALLBACK')
+        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/' + movie.id + '.json?page_limit=50&apikey=ah3k28e92uw29wedz8qgrsge&callback=JSON_CALLBACK')
         .success(function(movie) {
             console.log(movie);
             $modal.open({
@@ -32,7 +30,8 @@ function StixController($scope, $http, $modal) {
 
     var allMoviesPage = 1;
     $scope.page = function() {
-        if ( !$scope.activeTab['All Movies'] )
+        console.log('page');
+        if ( !$scope.data )
             return;
 
         $http.get('/all_movies?page=' + ++allMoviesPage)
